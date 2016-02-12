a4869854da2c88d2d2be53676a61c305d7cbc756
yurifrl <yurifrl@hotmail.com>
2016-02-12 15:15:35 -0200

    Isolate packages

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'yebo-ember-checkouts'
+  name: 'yebo-ember-checkouts',
+  // isDevelopingAddon: function() {
+  //   return true;
+  // },
+  included: function(app) {
+    this._super.included(app);
+    // app.import('vendor/ember-fsm.js');
+    app.import('vendor/register-checkouts.js');
+  }
 };
